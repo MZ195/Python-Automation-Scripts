a0e571b279de0a38b01b0b19c1e7042e2f09ddd1
Mazen.A <40984264+MZ195@users.noreply.github.com>
2020-09-16 19:37:49 +0300

    update

--- python-email-reading/Mail.py
+++ python-email-reading/Mail.py
@@ -7,8 +7,8 @@ import email
 import mailparser
 import json
 
-email_user = ""
-email_pass = ""
+email_user = "mazen-almazni@hotmail.com"
+email_pass = "almazni012013"
 host = "outlook.office365.com"
 
 mail = imaplib.IMAP4_SSL(host, 993)
@@ -21,7 +21,7 @@ print(type)
 mail_ids = data[0]
 id_list = mail_ids.split()
 
-with open("transactions.json", "w") as ff:
+with open("transactions2.json", "w") as ff:
     for num in data[0].split():
         current_transaction = {}
         transaction_details = []
@@ -73,68 +73,85 @@ with open("transactions.json", "w") as ff:
         transaction_details = [
             ele for ele in transaction_details if (ele not in ("", " "))]
 
-        # Extract Date and Time details
-        date_index = transaction_details.index('Date')
-        date_object = transaction_details[date_index + 1]
-        date_value = date_object.split(' ')[0]
-        time_value = date_object.split(' ')[1]
-        current_transaction["Date"] = date_value
-        current_transaction["Time"] = time_value
+        if(len(transaction_details) > 0):
+            if "Date" in transaction_details:
+                # Extract Date and Time details
+                date_index = transaction_details.index('Date')
+                date_object = transaction_details[date_index + 1]
+                date_value = date_object.split(' ')[0]
+                time_value = date_object.split(' ')[1]
+                current_transaction["Date"] = date_value
+                current_transaction["Time"] = time_value
+            else:
+                current_transaction["Date"] = "N/A"
+                current_transaction["Time"] = "N/A"
 
-        # Extract Transaction Reference
-        transaction_ref_index = transaction_details.index('Transaction_Ref')
-        transaction_ref_object = transaction_details[transaction_ref_index + 1]
-        current_transaction["Transaction_Ref"] = transaction_ref_object
+            if "Transaction_Ref" in transaction_details:
+                # Extract Transaction Reference
+                transaction_ref_index = transaction_details.index(
+                    'Transaction_Ref')
+                transaction_ref_object = transaction_details[transaction_ref_index + 1]
+                current_transaction["Transaction_Ref"] = transaction_ref_object
+            else:
+                current_transaction["Transaction_Ref"] = "N/A"
 
-        # Extract Amount and currency
-        amount_index = transaction_details.index('Amount')
-        amount_object = transaction_details[amount_index + 1]
-        amount_value = amount_object.split(' \xa0 (')[0]
-        currency_value = amount_object.split(' \xa0 (')[1][:-1].strip()
-        current_transaction["Amount"] = amount_value
-        current_transaction["Currency"] = currency_value
+            if "Amount" in transaction_details:
+                # Extract Amount and currency
+                amount_index = transaction_details.index('Amount')
+                amount_object = transaction_details[amount_index + 1]
+                amount_value = amount_object.split(' \xa0 (')[0]
+                currency_value = amount_object.split(' \xa0 (')[1][:-1].strip()
+                current_transaction["Amount"] = amount_value
+                current_transaction["Currency"] = currency_value
+            else:
+                current_transaction["Amount"] = "N/A"
+                current_transaction["Currency"] = "N/A"
 
-        # Extract the transaction Type and account
-        type_index = transaction_details.index('Type')
-        type_object = transaction_details[type_index + 1].split()[0]
-        account_object = transaction_details[type_index + 2].split()[0]
-        current_transaction["Type"] = type_object
-        current_transaction["Account"] = account_object
+            if "Type" in transaction_details:
+                # Extract the transaction Type and account
+                type_index = transaction_details.index('Type')
+                type_object = transaction_details[type_index + 1].split()[0]
+                account_object = transaction_details[type_index + 2].split()[0]
+                current_transaction["Type"] = type_object
+                current_transaction["Account"] = account_object
+            else:
+                current_transaction["Type"] = "N/A"
+                current_transaction["Account"] = "N/A"
 
-        # Extract Card number
-        card_index = transaction_details.index('Card')
-        card_object = transaction_details[card_index + 1].strip()
-        current_transaction["Card"] = card_object
+            # Extract Card number
+            if "Card" in transaction_details:
+                card_index = transaction_details.index('Card')
+                card_object = transaction_details[card_index + 1].strip()
+                current_transaction["Card"] = card_object
+            else:
+                current_transaction["Card"] = "N/A"
 
-        #  Extract Country information
-        country_index = transaction_details.index('Country')
-        country_object = transaction_details[country_index + 1].split('-')[
-            0].strip()
-        current_transaction["Country"] = country_object
+            if "Country" in transaction_details:
+                #  Extract Country information
+                country_index = transaction_details.index('Country')
+                country_object = transaction_details[country_index + 1].split('-')[
+                    0].strip()
+                current_transaction["Country"] = country_object
+            else:
+                current_transaction["Country"] = "N/A"
 
-        # Extract description
-        description_index = transaction_details.index('Transaction Details')
-        description_object = transaction_details[description_index + 1].split(' - ')[
-            0].strip()
-        current_transaction["Description"] = description_object
+            if "Transaction Details" in transaction_details:
+                # Extract description
+                description_index = transaction_details.index(
+                    'Transaction Details')
+                description_object = transaction_details[description_index + 1].split(' - ')[
+                    0].strip()
+                if(len(transaction_details) != 25):
+                    current_transaction["Description"] = description_object
+                else:
+                    current_transaction["Description"] = "needs_user_input"
+            else:
+                current_transaction["Description"] = "N/A"
 
-        res = json.dumps(current_transaction)
-
-        ff.write(res)
-        ff.write(", \n")
+            res = json.dumps(current_transaction)
+            ff.write(res)
+            ff.write(", \n")
 
         # print(current_transaction)
 
 exit()
-
-# for i in range(1, 9):
-#     del details_list[i*2]
-
-# final_json = {}
-
-# for i in range(0, 8):
-#     if i % 2 == 0:
-#         final_json[details_list[i]] = details_list[i+1]
-
-# print(final_json)
-# details_list = []
